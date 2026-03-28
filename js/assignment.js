7cffafb8d90e4574226426248fed18a3dafebd59
hartik teetwal <162800633+hartikteetwal@users.noreply.github.com>
2026-03-28 10:05:56 +0530

    hbgh

---
+++ js/assignment.js
@@ -0,0 +1,82 @@
+// odd even 
+
+// var N = 50
+// for (let i = 0; i <= N; i++){
+//     if (i%2 == 0) {
+//         console.log("Even ",i)
+//     } else {
+//         console.log("Odd ",i)
+//     }
+// }
+
+
+// sum of number 
+
+// var n = 5
+// var sum = 0
+// for (let i = 1; i <= n; i++) {
+//     sum = sum + i
+// }
+// console.log("sum = ", sum)
+
+
+// array iteration 
+
+// let arr = [2, 3, 4, 5, 6, 7]
+// // console.log(arr.length)
+// for (let i = 0; i <= arr.length - 1; i++){
+//     if (arr[i] === 6) {
+//         console.log(i)
+//     }
+// }
+
+
+// arr = [2, 5, 23,"harsh", NaN,77, 33,"hartik",undefined,null,0]
+// var limit = arr.length-1
+// // console.log(N)
+// let i;
+// for (i = 0; i <= limit; i++){
+//     // if (typeof(arr[i])==="string"){
+//     //     console.log(arr[i])
+//     // }
+
+//     if (!!arr[i] === true) {
+//         console.log(arr[i])
+//     }
+// }
+   
+
+// var obj = { name: "hartik", roll: 23, subject: "ABC", college: "XYZ" }
+// // keyArray = ["name","roll","subject","college"]
+// // console.log(obj.name)
+
+// let keyArray =  Object.keys(obj)
+// for (let i = 0; i <= keyArray.length - 1; i++){
+//     console.log(obj[keyArray[i]])
+// }
+
+
+
+// object iteration
+
+// var data = [
+//     {name:"subham",roll:1},
+//     {name:"mayank",roll:null},
+//     {name:"harsh",roll:34},
+//     {name:"hartik",roll:4},
+//     {name:"prince",roll:5}
+// ]
+
+// var limit = data.length-1
+// for (let i = 0; i <= limit; i++){ 
+
+//     // console.log(data[i])
+
+//     // if (data[i].name === "harsh") {
+//     //     console.log(data[i].roll)
+//     // }
+
+//     if (data[i].roll) {
+//         console.log(data[i])
+//     }
+// }
